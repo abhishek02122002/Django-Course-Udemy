536f33137e018599c19f2e2cbcb60efa0643dbef
Abhishek Kumar Singh <abhisheks02129@gmail.com>
2025-09-01 00:22:26 +0530

    file upload added

--- Feedback/feedbacks/profiles/urls.py
+++ Feedback/feedbacks/profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("profiles/", views.CreateProfileView.as_view())
-]
+    path("profiles/", views.CreateProfileView.as_view(),name="profiles"),
+    path("list/",views.ProfileView.as_view())
+] 
--- Feedback/feedbacks/profiles/views.py
+++ Feedback/feedbacks/profiles/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.http import HttpResponseRedirect
+from .forms import ProfileForm 
+from .models import userProfileModel
+from django.views.generic import ListView
 # Create your views here.
+
+
+# def store_file(file):
+#     with open("temp/image.png","wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html",{
+            "form":form
+        })
 
     def post(self, request):
-        pass
+        submitted_form= ProfileForm(request.POST,request.FILES)
+
+        if submitted_form.is_valid():
+            profile=userProfileModel(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles/")
+
+        return render(request,"profiles/create_profile.html",{
+            "form":submitted_form
+        })
+        
+
+
+class ProfileView(ListView):
+    model =userProfileModel
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
